ac99d22dddd8797f88630b18d5925e8d91fd1a9b
AnnaDays <analuizadias389@gmail.com>
2024-09-05 19:42:38 -0300

    Estudos DIO

---
+++ manipulando-strings/string3.py
@@ -0,0 +1,10 @@
+nome = "Ana Luiza dos Santos Dias"
+
+print(nome[0])
+print(nome[:4])
+print(nome[-1])
+print(nome[4:10])
+print(nome[2:10])
+print(nome[10:2:-1])
+print(nome[:])
+print(nome[::-1])
---
+++ manipulando-strings/string4.py
@@ -0,0 +1,24 @@
+# STRING TRIPLA
+nome = 'Ana Luiza'
+
+mensagem = f"""
+   Olá meu nome é {nome},
+  Eu estou aprendendo Python.
+    Essa mensagem tem diferentes recuos.
+ """
+ 
+print(mensagem)
+
+print( 
+      """
+      *********** MENU ***********
+      
+      1- DEPOSITAR
+      2 - SACAR
+      0 - SAIR
+      
+      ****************************
+      OBRIGADO POR USAR NOSSO SISTEMA!!
+      
+      """
+      )
