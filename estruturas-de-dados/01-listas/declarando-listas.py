4c7cf7b05c940c1a4f92d0795cf4bdb2c1cf42c6
AnnaDays <analuizadias389@gmail.com>
2024-09-18 19:41:55 -0300

    Create declarando-listas.py

---
+++ estruturas-de-dados/01-listas/declarando-listas.py
@@ -0,0 +1,29 @@
+frutas = ["laranja", "maca", "uva", "pera"]
+print(frutas)
+print(frutas[0])
+print(frutas[1])
+print(frutas[-1])
+print(frutas[-3])
+
+frutas = []
+print(frutas)
+
+letras = list("python")
+print(letras)
+
+numeros = list(range(10))
+print(numeros)
+
+carro = ["Ferrari", "F8", 4200000, 2020, 2900, "São Paulo", True]
+print(carro)
+
+matriz = [
+    [1, "a", 2],
+    ["b", 3, 4],
+    [6, 5, "c"]
+]
+
+print(matriz[0])  # [1, "a", 2]
+print(matriz[0][0])  # 1
+print(matriz[0][-1])  # 2
+print(matriz[-1][-1])  # "c"
